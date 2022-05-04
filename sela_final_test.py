17034ec61fdd05ebebc6eb2257a33d1319997f20
c-laD <sdomkofski@olin.edu>
2022-05-04 15:06:27 -0400

    Adds functional dress up game with a few bugs

--- sela_final_test.py
+++ sela_final_test.py
@@ -1,5 +1,6 @@
 
 # Import and initialize the pygame library
+from matplotlib.collections import _CollectionWithSizes
 import pygame
 from pygame.locals import (
     K_UP,
@@ -11,15 +12,14 @@ from pygame.locals import (
 
 pygame.init()
 
-bottoms = ['sela_test_images/invisible.png', 'clothes/black_bottom.png', 'clothes/blue_bottom.png', 'clothes/creep_bottom.png', 'clothes/fish_bottom.png', 'clothes/flannel_bottom.png', \
-'clothes/grey_bottom.png', 'clothes/net_bottom.png', 'clothes/rainbow_bottom.png', 'clothes/red_bottom.png', 'clothes/thigh_bottom.png', 'clothes/yellow_bottom.png']
-bottom_coords = [(0,0), (10,500), (20,100), (30,0), (40,0), (50,0), (60,0), (70,0), (80,0), (90,0), (10,0), (100,0)]
-tops = ['sela_test_images/invisible.png', 'clothes/blue_top.png', 'clothes/flag_top.png', 'clothes/flannel_top.png', 'clothes/ocean_top.png', 'clothes/pearl_top.png', 'clothes/rainbow_top.png', \
-'clothes/red_top.png', 'clothes/tank_top.png', 'clothes/tie_top.png']
-top_coords = [(0,0), (10,500), (20,100), (30,0), (40,0), (50,0), (60,0), (70,0), (80,0), (90,0), (10,0), (100,0)]
-hats = ['sela_test_images/invisible.png', 'clothes/cap_hat.png', 'clothes/cat_hat.png', 'clothes/cow_hat.png', 'clothes/horn_hat.png', 'clothes/straw_hat.png', 'clothes/sun_hat.png', \
-'clothes/unicorn_hat.png']
-hat_coords = [(0,0), (10,500), (20,100), (30,0), (40,0), (50,0), (60,0), (70,0), (80,0), (90,0), (10,0), (100,0)]
+#change to dictionary? or a list of tuples?
+
+bottoms = [['sela_test_images/invisible.png', (0,0)], ['clothes/black_bottom.png', (159, 522)], ['clothes/blue_bottom.png', (142,524)], ['clothes/creep_bottom.png', (142,524)], ['clothes/fish_bottom.png', (115,524)], ['clothes/flannel_bottom.png', (142,524)], \
+['clothes/grey_bottom.png', (142,524)], ['clothes/net_bottom.png', (126,528)], ['clothes/rainbow_bottom.png', (142,524)], ['clothes/red_bottom.png', (179,529)], ['clothes/thigh_bottom copy.png', (109,526)], ['clothes/yellow_bottom.png', (142,524)]]
+tops = [['sela_test_images/invisible.png', (0,0)], ['clothes/blue_top.png', (176,520)], ['clothes/flag_top.png', (192,521)], ['clothes/flannel_top.png', (170,522)], ['clothes/ocean_top.png', (144,523)], ['clothes/pearl_top.png', (198,518)], ['clothes/rainbow_top.png', (177,522)], \
+['clothes/red_top.png', (199,537)], ['clothes/tank_top.png', (190,522)], ['clothes/tie_top.png', (240,541)]]
+hats = [['sela_test_images/invisible.png', (0,0)], ['clothes/cap_hat.png', (205,435)], ['clothes/cat_hat.png', (190,457)], ['clothes/cow_hat.png', (160,430)], ['clothes/horn_hat.png', (205,432)], ['clothes/straw_hat.png', (165,437)], ['clothes/sun_hat.png', (199,490)], \
+['clothes/unicorn_hat.png', (236,440)]]
 
 
 i_hat = 0
@@ -37,54 +37,70 @@ class Outfit():
 
 
     def __init__(self):
-        self.bottom = bottoms[0]
-        self.top = tops[0]
-        self.hat = hats[0]
-        self.bottom_coords_index = 0
-        self.top_coords_index = 0
-        self.hat_coords_index = 0
+        self.bottom_set = bottoms[0]
+        self.top_set = tops[0]
+        self.hat_set = hats[0]
+        self.bottom = self.bottom_set[0]
+        self.top = self.top_set[0]
+        self.hat = self.hat_set[0]
+        self.bottom_coords = self.bottom_set[1]
+        self.top_coords = self.top_set[1]
+        self.hat_coords = self.hat_set[1]
+        self.clothes_set = ''
 
     def change_right(self, index, clothes_type):
         self.clothes_list = clothes_type
         if clothes_type == hats:
             new_index = outfit.set_change_right(index)
-            self.hat = self.clothes_list[index]
-            self.hat_coords_index = new_index
+            self.clothes_set = hats
+            self.clothes_list = self.clothes_set[new_index]
+            self.hat = self.clothes_list[0] #calls first item, the name string
+            self.hat_coords = self.change_coords(new_index)
         if clothes_type == tops:
             new_index = outfit.set_change_right(index)
-            self.top = self.clothes_list[index]
-            self.top_coords_index = new_index
+            self.clothes_set = tops
+            self.clothes_list = self.clothes_set[new_index]
+            self.top = self.clothes_list[0]
+            self.top_coords = self.change_coords(new_index)
         if clothes_type == bottoms:
             new_index = outfit.set_change_right(index)
-            self.bottom = self.clothes_list[index]
-            self.bottom_coords_index = new_index
+            self.clothes_set = bottoms
+            self.clothes_list = self.clothes_set[new_index]
+            self.bottom = self.clothes_list[0]
+            self.bottom_coords = self.change_coords(new_index)
         return new_index
 
     def set_change_right(self, index):
-        if index + 1 >= len(self.clothes_list):
+        if index + 1 >= len(self.clothes_set):
             index = 0
         else: index += 1
         return index
 
+# editing to account for calling in lists to lists
     def change_left(self, index, clothes_type):
-        self.clothes_list = clothes_type
         if clothes_type == hats: 
             new_index = outfit.set_change_left(index)
-            self.hat = self.clothes_list[index]
-            self.hat_coords_index = new_index
+            self.clothes_set = hats
+            self.clothes_list = self.clothes_set[new_index]
+            self.hat_coords = self.change_coords(new_index)
+            self.hat = self.clothes_list[0] #calls first item, the name string
         if clothes_type == tops:
             new_index = outfit.set_change_left(index)
-            self.top = self.clothes_list[index]
-            self.top_coords_index = new_index
+            self.clothes_set = tops
+            self.clothes_list = self.clothes_set[new_index]
+            self.top = self.clothes_list[0]
+            self.top_coords = self.change_coords(new_index)
         if clothes_type == bottoms: 
             new_index = outfit.set_change_left(index)
-            self.bottom = self.clothes_list[index]
-            self.bottom_coords_index = new_index
+            self.clothes_set = bottoms
+            self.clothes_list = self.clothes_set[new_index]
+            self.bottom = self.clothes_list[0]
+            self.bottom_coords = self.change_coords(new_index)
         return new_index
 
     def set_change_left(self, index):
-        if index == 0:
-            index = len(self.clothes_list) - 1
+        if index == 0 or index == -8:
+            index = len(self.clothes_set) - 1
         else: index -= 1
         return index
 
@@ -92,11 +108,14 @@ class Outfit():
         bottom_image = pygame.image.load(self.bottom)
         top_image = pygame.image.load(self.top)
         hat_image = pygame.image.load(self.hat)
-        # screen.blit(bottom_image, bottom_coords[self.bottom_coords_index])
-        screen.blit(bottom_image, (0,0))
-        screen.blit(top_image, top_coords[self.top_coords_index])
-        screen.blit(hat_image, hat_coords[self.hat_coords_index])
+        screen.blit(bottom_image, self.bottom_coords)
+        screen.blit(top_image, self.top_coords)
+        screen.blit(hat_image, self.hat_coords)
 
+    def change_coords(self, index):
+        current_set = self.clothes_set[index]
+        coords = current_set[1]
+        return coords
 
 
 # Set up the drawing window
@@ -116,6 +135,7 @@ while running:
             running = False
         outfit.show_clothes()
         if event.type == KEYDOWN:
+            # set instruction screen visible to false says press right arrow to continue
             pressed_keys = pygame.key.get_pressed()
             if pressed_keys[K_UP]:
                 if selector == 0: selector = 2
@@ -133,15 +153,12 @@ while running:
                 if selector == 2: i_bottom = outfit.change_left(i_bottom, clothing)
                 if selector == 1: i_top = outfit.change_left(i_top, clothing)
                 if selector == 0: i_hat = outfit.change_left(i_hat, clothing)
-            #selector_list[selector[1]] = i
+            #if enter key is pressed: move on to platformer
 
-            print(selector)
-            print(i_hat, i_bottom, i_top)
     
     screen.blit(background, (0,0))
     screen.blit(foreground, (0,0))
     screen.blit(pygame.image.load(arrow_list[selector]), (0,0))
-    #print(outfit.bottom)
 
     outfit.show_clothes()
     
